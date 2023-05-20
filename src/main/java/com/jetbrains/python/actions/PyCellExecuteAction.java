826bc3db885807f31739363d0f55102a9f24955d
guoci <zguoci@gmail.com>
2023-05-20 13:54:45 -0400

    add "Execute cell in console" feature

---
+++ src/main/java/com/jetbrains/python/actions/PyCellExecuteAction.java
@@ -0,0 +1,111 @@
+package com.jetbrains.python.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.LogicalPosition;
+import com.intellij.openapi.editor.ScrollType;
+import com.intellij.openapi.editor.SelectionModel;
+import com.intellij.openapi.editor.VisualPosition;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiWhiteSpace;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.DocumentUtil;
+import com.jetbrains.python.PyTokenTypes;
+import com.jetbrains.python.psi.PyElementType;
+import com.jetbrains.python.psi.PyElsePart;
+import com.jetbrains.python.psi.PyExceptPart;
+import com.jetbrains.python.psi.PyFile;
+import com.jetbrains.python.psi.PyFinallyPart;
+import com.jetbrains.python.psi.PyIfPart;
+import com.jetbrains.python.psi.PyStatementList;
+import com.jetbrains.python.psi.PyStatementPart;
+import com.jetbrains.python.psi.PyWhilePart;
+import com.jetbrains.python.psi.impl.PyIfPartElifImpl;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class PyCellExecuteAction extends AnAction {
+
+  private static void cellExecute(final AnActionEvent e, final Editor editor) {
+    final Document document = editor.getDocument();
+    final PsiDocumentManager psiDocumentManager = PsiDocumentManager.getInstance(e.getProject());
+    psiDocumentManager.commitDocument(document);
+    final PsiFile psiFile = psiDocumentManager.getPsiFile(document);
+
+    final LogicalPosition logicalPos = editor.getCaretModel().getLogicalPosition();
+    final int line = logicalPos.line;
+
+    int start = 6666, end = 6666;
+    for (int i = 0; i > -666; --i) {
+      final int cline = line + i;
+      int offset = 666;
+      boolean outOfDoc = false;
+      try {
+        offset = DocumentUtil.getFirstNonSpaceCharOffset(document, cline);
+        start = offset;
+      } catch (IndexOutOfBoundsException ex) {
+        outOfDoc = true;
+      }
+      PsiElement psiElement = null;
+      if (!outOfDoc) {
+        psiElement = psiFile.findElementAt(offset);
+        if (outOfDoc || !(psiElement.getNode().getElementType() instanceof PyElementType)) {
+          break;
+        }
+      }
+//      System.out.println("i = " + i);
+//      System.out.println("cline = " + cline);
+//      System.out.println("psiElement = " + psiElement);
+    }
+//    System.out.println("forward");
+    for (int i = 0; i < 666; ++i) {
+      final int cline = line + i;
+      int offset = 666;
+      boolean outOfDoc = false;
+      try {
+        offset = DocumentUtil.getFirstNonSpaceCharOffset(document, cline);
+        end = offset;
+      } catch (IndexOutOfBoundsException ex) {
+        outOfDoc = true;
+      }
+      PsiElement psiElement = null;
+      if (!outOfDoc) {
+        psiElement = psiFile.findElementAt(offset);
+        if (outOfDoc || !(psiElement.getNode().getElementType() instanceof PyElementType)) {
+          break;
+        }
+      }
+//      System.out.println("i = " + i);
+//      System.out.println("cline = " + cline);
+//      System.out.println("psiElement = " + psiElement);
+    }
+    {
+      start = DocumentUtil.getLineEndOffset(start, document);
+      end = DocumentUtil.getLineEndOffset(end, document);
+      String codeToSend = editor.getDocument().getCharsSequence().subSequence(start, end).toString();
+//      System.out.println("codeToSend = " + codeToSend);
+      PyExecuteInConsole.executeCodeInConsole(e.getProject(), codeToSend, null, true, true, false,
+          null);
+    }
+  }
+
+  @Override
+  public void actionPerformed(@NotNull AnActionEvent e) {
+    Editor editor = CommonDataKeys.EDITOR.getData(e.getDataContext());
+    if (editor != null) {
+      final String selectionText = PySmartExecuteSelectionAction.getSelectionText(editor);
+      if (selectionText != null) {
+        PyExecuteInConsole.executeCodeInConsole(e.getProject(), selectionText, null, true, true, false, null);
+      }
+      else {
+        cellExecute(e, editor);
+      }
+    }
+  }
+}
